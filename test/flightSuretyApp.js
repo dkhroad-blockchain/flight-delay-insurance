dfbbb85c9c14ee5205b3d865feecde6e9dabe31b
Devinder Khroad <dkhroad@gmail.com>
2019-09-05 13:48:09 -0700

    streamline tests to spend fewer ether

--- test/flightSuretyApp.js
+++ test/flightSuretyApp.js
@@ -5,30 +5,43 @@ const FlightSuretyData = artifacts.require('FlightSuretyData');
 contract('Flight Surety App Tests', async (accounts) => {
 
   before(async () => {
+    accounts.forEach(async (a,i) => {
+      console.log(`accounts[${i}]: ${a}, balance:`,await web3.eth.getBalance(a));
+    });
+    this.accounts = accounts;
+    this.flightSuretyData = await FlightSuretyData.new();
+    this.flightSuretyApp = await FlightSuretyApp.new(this.flightSuretyData.address);
+  });
 
-    accounts.forEach((a,i) => {
-      console.log(`accounts[${i}]: ${a}`);
+  after(async () => {
+    accounts.forEach(async (a,i) => {
+      console.log(`accounts[${i}]: ${a}, balance:`,await web3.eth.getBalance(a));
     });
   });
 
   beforeEach('setup contract', async () => {
-    this.accounts = accounts;
-    this.flightSuretyData = await FlightSuretyData.new();
-    this.flightSuretyApp = await FlightSuretyApp.new(this.flightSuretyData.address);
+    // this.accounts = accounts;
+    // this.flightSuretyData = await FlightSuretyData.new();
+    // this.flightSuretyApp = await FlightSuretyApp.new(this.flightSuretyData.address);
 
-    await this.flightSuretyApp.setAirlineMinimumFunds(web3.utils.toWei("2","ether"));
-    await this.flightSuretyData.authorizeContract(this.flightSuretyApp.address);
-    await this.flightSuretyApp.bootstrap("A0",{value: web3.utils.toWei("2","ether")});
+    console.log("app contract balance: ", await web3.eth.getBalance(this.flightSuretyApp.address));
+    console.log("data contract balance: ", await web3.eth.getBalance(this.flightSuretyData.address));
+    // await this.flightSuretyApp.setAirlineMinimumFunds(web3.utils.toWei("2","ether"));
+    // await this.flightSuretyData.authorizeContract(this.flightSuretyApp.address);
+    // await this.flightSuretyApp.bootstrap("A0",{value: web3.utils.toWei("2","ether")});
+
+    // this.flightSuretyApp.sendTransaction({value: web3.utils.toWei("15","ether"), from: this.accounts[9]});
+    // this.flightSuretyData.sendTransaction({value: web3.utils.toWei("15","ether"), from: this.accounts[9]});
     // await this.flightSuretyApp.registerAirline(this.accounts[0],"A1");      
     // await this.flightSuretyApp.fund(this.accounts[0],{value: web3.utils.toWei("2","ether")});
   });
 
 
   describe("as a non-authorized contract", async () => {
-    beforeEach(async () => {
-      await this.flightSuretyData.deAuthorizeContract(this.flightSuretyApp.address);
-      await this.flightSuretyData.authorizeContract(this.accounts[5]);
-    });
+    // beforeEach(async () => {
+    //   await this.flightSuretyData.deAuthorizeContract(this.flightSuretyApp.address);
+    //   await this.flightSuretyData.authorizeContract(this.accounts[5]);
+    // });
 
     it("cannot call functions in data contract", async () => {
       let newAirline = this.accounts[2];
@@ -42,43 +55,26 @@ contract('Flight Surety App Tests', async (accounts) => {
   });
 
   describe("as a authorized contract", async () => {
+    before(async () => {
+      await this.flightSuretyData.authorizeContract(this.flightSuretyApp.address);
+      await this.flightSuretyApp.setAirlineMinimumFunds(web3.utils.toWei("2","ether"));
+      await this.flightSuretyApp.bootstrap("A0",{value: web3.utils.toWei("2","ether")});
+    });
+
     describe('an airline',async () => {
+
+      it("can be registerd by a funded airline", async () => {
+        let {logs} =  await this.flightSuretyApp.registerAirline(this.accounts[1],"A1");
+        expectEvent.inLogs(logs,"AirlineRegistered",{airline: this.accounts[1]});
+      });
+
       describe('when registered', async() => {
-        beforeEach(async () => {
+        before(async () => {
           // register a new airline
-          await this.flightSuretyApp.registerAirline(this.accounts[1],"A1");
+          // await this.flightSuretyApp.registerAirline(this.accounts[1],"A1");
         });
 
-        it("can fund itself", async () => {
-            let {logs} = await  this.flightSuretyApp.fund(this.accounts[1],
-              {
-                value: web3.utils.toWei("2","ether"),
-                from: this.accounts[1]
-              });
-            expectEvent.inLogs(logs,"AirlineFunded",{airline: this.accounts[1]});
-
-        });
-
-        describe('and funded', async () => {
-
-          beforeEach(async () => {
-            // fund new airline
-            await this.flightSuretyApp.fund(
-              this.accounts[1],
-              {
-                value: web3.utils.toWei("2","ether"),
-                from: this.accounts[1]
-              });
-          });
-
-          it("can register a new airline", async () => {
-            let {logs} = await  this.flightSuretyApp.registerAirline(this.accounts[2],"A2",{from: this.accounts[1]});
-            expectEvent.inLogs(logs,"AirlineRegistered",{airline: this.accounts[2]});
-          });
-
-        });
-
-        describe('not funded', async () => {
+        describe('but not funded', async () => {
           it("cannot register an airline",async () => {
             let newAirline1 = this.accounts[1];
             let newAirline2 = this.accounts[2];
@@ -87,36 +83,40 @@ contract('Flight Surety App Tests', async (accounts) => {
               "Caller is not funded"
             );
           });
+        }); 
+
+
+        describe('when funded', async () => {
+          it("can fund itself", async () => {
+            let {logs} = await  this.flightSuretyApp.fund(this.accounts[1],
+              {
+                value: web3.utils.toWei("2","ether"),
+                from: this.accounts[1]
+              });
+            expectEvent.inLogs(logs,"AirlineFunded",{airline: this.accounts[1]});
+          });
+          it("can register a new airline", async () => {
+            let {logs} = await  this.flightSuretyApp.registerAirline(this.accounts[2],"A2",{from: this.accounts[1]});
+            expectEvent.inLogs(logs,"AirlineRegistered",{airline: this.accounts[2]});
+          });
         });
       });
 
       describe('when 5 or more airlines are registered', async () => {
-        beforeEach(async () => {
-        });
         it("requires 50% of funded airlines to register a new airline", async () => {
-
-          // this.accounts.slice(1,2).forEach( async (account,idx) => { 
-          //   console.log("account[" + idx + "] =" + account + ": registering");
-          //   await this.flightSuretyApp.registerAirline(account,"AA");
-          //   console.log("account[" + idx + "] =" + account + ": funding");
-          //   await this.flightSuretyApp.fund(account,
-          //     { value: web3.utils.toWei("2","ether"), 
-          //       from: account }
-          //   );
-          // });
           let tx;
 
-          tx = await this.flightSuretyApp.registerAirline(this.accounts[1],"A1");
-          expectEvent.inLogs(tx.logs,"AirlineRegistered",{airline: this.accounts[1]});
+          // tx = await this.flightSuretyApp.registerAirline(this.accounts[1],"A1");
+          // expectEvent.inLogs(tx.logs,"AirlineRegistered",{airline: this.accounts[1]});
 
-          tx = await this.flightSuretyApp.fund(this.accounts[1], { 
-            value: web3.utils.toWei("2","ether"), 
-            from: this.accounts[1] 
-          });
-          expectEvent.inLogs(tx.logs,"AirlineFunded",{airline: this.accounts[1]});
+          // tx = await this.flightSuretyApp.fund(this.accounts[1], { 
+          //   value: web3.utils.toWei("2","ether"), 
+          //   from: this.accounts[1] 
+          // });
+          // expectEvent.inLogs(tx.logs,"AirlineFunded",{airline: this.accounts[1]});
 
-          tx = await this.flightSuretyApp.registerAirline(this.accounts[2],"A2");
-          expectEvent.inLogs(tx.logs,"AirlineRegistered",{airline: this.accounts[2]});
+          // tx = await this.flightSuretyApp.registerAirline(this.accounts[2],"A2");
+          // expectEvent.inLogs(tx.logs,"AirlineRegistered",{airline: this.accounts[2]});
 
           tx = await this.flightSuretyApp.fund(this.accounts[2], { 
             value: web3.utils.toWei("2","ether"), 
@@ -143,28 +143,28 @@ contract('Flight Surety App Tests', async (accounts) => {
           expectEvent.inLogs(tx.logs,"AirlineFunded",{airline: this.accounts[4]});
           expectEvent.inLogs(tx.logs,"RequirementChanged",{current: new BN(2)});
 
+          // airline A6 is registered but not funded. Therefore doesn't take part in consensus
+          tx = await this.flightSuretyApp.registerAirline(this.accounts[6],"A6");
+          tx = await this.flightSuretyApp.registerAirline(this.accounts[6],"A6",{from: this.accounts[2]});
+          expectEvent.inLogs(tx.logs,"AirlineRegistered",{airline: this.accounts[6]});
+
           await this.flightSuretyApp.registerAirline(this.accounts[5],"A5");
           tx = await this.flightSuretyApp.registerAirline(this.accounts[5],"A5",{from: this.accounts[1]});
           expectEvent.inLogs(tx.logs,"AirlineRegistered",{airline: this.accounts[5]});
-
-        });
-
-        xit("A registered but not funded airline does not participate in the contract (multi-party consensus)", async () => {
         });
       });
 
       describe('when an airline is not registered', async() => {
         it("cannot register an another airline",async () => {
-          let newAirline1 = this.accounts[8];
-          let newAirline2 = this.accounts[9];
+          let newAirline8 = this.accounts[8];
+          let newAirline9 = this.accounts[9];
           await expectRevert(
-            this.flightSuretyApp.registerAirline(newAirline2,"A2",{from: newAirline1 } ),
+            this.flightSuretyApp.registerAirline(newAirline8,"A8",{from: newAirline9 } ),
             "Caller is not registered"
           );
         });
       });
-    });
 
-  });
-
-});
+    }); // an airline 
+  }); // authorized contract
+}); // contract
